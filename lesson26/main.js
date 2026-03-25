540a78e0343cd852e654c23adf29f4cad50860de
unknown <Student@DIGITALSCHOOL.LOCAL>
2026-03-25 16:16:15 +0100

    commit

---
+++ lesson26/main.js
@@ -0,0 +1,30 @@
+console.log("Goodbye");
+
+var x = 5;
+
+var a = 50 + 50;
+console.log(a);
+
+var b = 20;
+
+var c = 30;
+
+var d = b + c;
+
+console.log(d);
+
+var e = 40;
+
+var f = 10;
+
+var g = e - f;
+
+console.log(g);
+
+var h = 10;
+
+var j = 4;
+
+var k = h - j;
+
+console.log(k);
