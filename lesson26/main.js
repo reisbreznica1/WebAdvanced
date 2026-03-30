d690056b481ed59b996cdb88953a6c6b469610d3
reisbreznica1 <reis.breznica1@gmail.com>
2026-03-30 16:15:02 +0200

    commit

--- lesson26/main.js
+++ lesson26/main.js
@@ -5,6 +5,7 @@ var x = 5;
 var a = 50 + 50;
 console.log(a);
 
+
 var b = 20;
 
 var c = 30;
@@ -12,6 +13,7 @@ var c = 30;
 var d = b + c;
 
 console.log(d);
+
 
 var e = 40;
 
@@ -21,6 +23,7 @@ var g = e - f;
 
 console.log(g);
 
+
 var h = 10;
 
 var j = 4;
@@ -28,3 +31,113 @@ var j = 4;
 var k = h - j;
 
 console.log(k);
+
+
+var l = 99;
+
+l++;
+
+console.log(l);
+
+
+var o = 99;
+
+o--;
+
+console.log(o);
+
+
+var r = 20;
+
+var e = r ** 2;
+
+console.log(e);
+
+
+var i = 15;
+
+i+=5
+
+console.log(i);
+
+
+var s = 15;
+
+s-=5
+
+console.log(s);
+
+
+var q = 15;
+
+q*=5
+
+console.log(q);
+
+
+var w = 15;
+
+w/=5
+
+console.log(w);
+
+
+var t = 1;
+
+var y = 1;
+
+console.log(t == y);
+
+
+var u = 1;
+
+var i = 2;
+
+console.log(t != y);
+
+
+var num1 = 1;
+
+var num2 = 2;
+
+console.log(num1 > num2);
+
+
+var num3 = 10;
+
+var num4 = 100;
+
+console.log(num3 > num4);
+
+
+var num5 = 123;
+
+var num6 = 123;
+
+console.log(num5 === num6);
+
+
+var firstName = "Reis"
+
+var lastName = "Breznica"
+
+var fullName = firstName + " " + lastName;
+
+console.log(fullName);
+
+
+var school = "Digital School"
+
+console.log(`Hello from ${school}`);
+
+
+var age = 25;
+
+console.log(age >= 15 && age <= 65);
+
+
+var var1 = 5;
+
+var var2 = 6;
+
+console.log(var1 == 5 || var2 == 5)
---
+++ lesson27/main.js
@@ -0,0 +1,11 @@
+function showMessage(){
+    alert("This massage is inside the function")
+}
+
+showMessage()
+
+function sum(number1, number2){
+    return number1 + number2;
+}
+
+console.log(sum(25,5))
