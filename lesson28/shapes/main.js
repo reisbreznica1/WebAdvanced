9b8269da8825e997c21104e9df13d5925d43de09
reisbreznica1 <reis.breznica1@gmail.com>
2026-04-15 16:04:39 +0200

    commit

---
+++ lesson28/shapes/main.js
@@ -0,0 +1,25 @@
+var button1 = document.getElementById('btn1')
+var button2 = document.getElementById('btn2')
+var button3 = document.getElementById('btn3')
+var button4 = document.getElementById('btn4')
+
+button1.onclick = function()
+{
+    alert('HELLO')
+}
+
+button2.onmouseover = function()
+{
+    alert('HELLO')
+}
+
+button3.onmouseleave = function()
+{
+    alert('HELLO')
+}
+
+button4.onmousewheel = function()
+{
+    alert('HELLO')
+}
+
