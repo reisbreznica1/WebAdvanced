c7e09dea2d7c598f625735d53621cf02d33d41e1
reisbreznica1 <reis.breznica1@gmail.com>
2026-04-22 16:08:41 +0200

    commit

---
+++ lesson29.easy/main.js
@@ -0,0 +1,5 @@
+var nameofbooks = ["atomic habits", "rich dad poor dad", "48 laws of power", "the art of seduction", "the law of human natures"];
+
+var [firstbook, secondbook, thirdbook, fourthbook, fifthbook] = nameofbooks;
+
+console.log(secondbook);
