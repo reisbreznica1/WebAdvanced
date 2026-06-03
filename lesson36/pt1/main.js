2be5fc20d89275e26994f7defc37b38cffcc4abc
reisbreznica1 <reis.breznica1@gmail.com>
2026-06-03 16:12:16 +0200

    commit

---
+++ lesson36/pt1/main.js
@@ -0,0 +1,15 @@
+$(window).ready(function(){
+    alert('it is working')
+});
+
+$('#header');
+$('li');
+$('ul li');
+$('.student');
+
+$('a.test:first');
+$('tr:odd');
+$('#myform:input');
+$('div:visible');
+$('div:gt(2)');
+$('div:animated');
