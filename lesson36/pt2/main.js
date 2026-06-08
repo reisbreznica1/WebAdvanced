c6d7ec1a270ca4fc2353ca31fad18a0685f436e1
reisbreznica1 <reis.breznica1@gmail.com>
2026-06-08 16:16:55 +0200

    commit

--- lesson36/pt2/main.js
+++ lesson36/pt2/main.js
@@ -5,24 +5,47 @@ $('li').click(function(){
 $('#btn').click(function(){
     console.log($('#h1').text());
     $('#h1').text('Testing text');
-    $('#h1').append('Extra text');
+     $('#h1').append('Extra text');
 });
 
-$('btn2').on('click', function(){
+$('#btn2').on('click', function(){
     console.log('Second button is clicked');
 });
 
 $('#myDiv').click(function(){
     $(this).addClass('clicked');
-});
+})
 .find('span')
 .attr('title', 'Hover over me');
 
-$('btn3').clcik(function(){
+$('#btn3').click(function(){
     $(".hidden").show('slow');
 });
 
-$('btn4').clcik(function(){
+$('#btn4').click(function(){
     $(".hidden").hide();
 });
 
+
+
+$('#square').click(function(){
+
+    $('#square').animate({
+        'width': '200px',
+        'height': '200px'
+    });
+});
+
+$('.testing').click(function(){
+
+    $('.testing').animate({
+        'left': '+=50',
+        'opacity': 0.25,
+        'fontSize': '30px'
+    },
+    300,
+    function(){
+        console.log("Animation is complete")
+    }
+);
+});
